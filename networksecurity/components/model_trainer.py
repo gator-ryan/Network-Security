4d13a242b19c69121bff8eb31ba50567a7e3afa9
Narayan Singh Bhadauriya <professionalsinghbhadauriya@gmail.com>
2025-10-06 16:16:24 +0530

    Pipeline for training and batch prediction done

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -150,6 +150,7 @@ class ModelTrainer:
 
         Network_Model=NetworkModel(preprocessor=preprocessor,model=best_model)
         save_object(self.model_trainer_config.trained_model_file_path,obj=NetworkModel)
+        
         #model pusher
         save_object("final_model/model.pkl",best_model)
         
